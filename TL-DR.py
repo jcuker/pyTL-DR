c738c7848363b80c23b84f199ee8ce68e73938c1
Jordan Cuker <eliteforcejac@gmail.com>
2018-01-20 15:04:40 -0600

    html being generated and opened in broswer

--- TL-DR.py
+++ TL-DR.py
@@ -1,13 +1,17 @@
 import requests
 import json
-# import pystache
+import pystache
+import os
+import dateutil.parser
+import calendar
+import webbrowser
 
 class ArticleInformation(object):
 
     # member variables
     # title, url, content, author, imageUrl, publishedAt
 
-    summarizedContent = "SMMRY has not yet been called."
+    summarizedContent = None
 
     def __init__(self, title, url, content, author, imageUrl, publishedAt):
         if title is None:
@@ -39,7 +43,8 @@ class ArticleInformation(object):
         print("Title: " + self.title)
         print("Url: " + self.url)
         print("Content: " + self.content)
-        print("SummarizedContent: " + self.summarizedContent)
+        if self.summarizedContent is not None:
+            print("SummarizedContent: " + self.summarizedContent)
         print("Author: " + self.author)
         print("ImageUrl: " + self.imageUrl)
         print("PublishedAt: " + self.publishedAt)
@@ -122,14 +127,71 @@ def SummarizeArticleList(articleInformationList):
     
     return
 
+def ReadInFile(filename):
+    f = open(os.getcwd() + filename, "r")
+    fileAsString = f.read()
+    f.close()
+    return fileAsString
+
+def GenerateCardFromArticleInformation(articleInformation):
+    cardHTML = ReadInFile("/html/card.html")
+    renderer = pystache.Renderer()
+    preParsed = pystache.parse(cardHTML)
+
+    content = articleInformation.content
+    if articleInformation.summarizedContent is not None:
+        content = articleInformation.summarizedContent
+
+    date = dateutil.parser.parse(articleInformation.publishedAt)
+    day = date.day
+    month = calendar.month_name[date.month]
+
+    renderedCardHTML = renderer.render(preParsed,{'url' : articleInformation.url}, {'title' : articleInformation.title}, {'author' : articleInformation.author}, {'content' : content}, {'day' : day}, {'month' : month}, {'imageUrl' : articleInformation.imageUrl})
+    return renderedCardHTML
+
+def SaveToFile(path, content):
+    f = open(os.getcwd() + path, 'w')
+    f.write(content)
+    f.close()
+    return
+
+def InsertCardIntoHTMLDoc(card, more = False):
+    path = "/html/main.html"
+    htmlFile = ReadInFile(path)
+    renderer = pystache.Renderer()
+    preParsed = pystache.parse(htmlFile)
+
+    renderedHTML = None
+    if more is True:
+        renderedHTML = renderer.render(preParsed, {'card' : card + '\n\n{{{card}}}'})
+    else:
+        renderedHTML = renderer.render(preParsed, {'card' : card})
+    
+    SaveToFile('/html/main.html', renderedHTML)
+
+def initHTML():
+    newHTML = "<html>\n<head><title>TL-DR</title>\n<link href=\"style.css\" rel=\"stylesheet\"/>\n<body>\n{{{card}}}\n</body>"
+    SaveToFile('/html/main.html', newHTML)
+    return
+
+def OpenInWebBrowser():
+    webbrowser.open_new_tab(os.getcwd() + '/html/main.html')
+
 def main():
     initKeys()
-
+    initHTML()
     articleInformationList = GetArticleInformationList()
 
-    for currentArticle in articleInformationList:
-        currentArticle.print()
+    #smmryResponse = SmmryAPIGet(articleInformationList[0].url, smmryApiKey)
+    articleInformationList[0].summarizedContent = "A group of gunmen have launched an attack on the Intercontinental Hotel in Kabul, authorities said Saturday. Interior ministry spokesman Nasrat Rahimi told CBS News that they believe four gunmen attacked the hotel. Rahimi did not provide details on casualties but said foreign and Afghan guests were inside the hotel during the attack. The attackers entered the hotel through its garden and then through the kitchen, Danish said, adding that the hotel's security was recently handed over to a private security company, but did not provide its name. \"We are aware of reports that extremist groups may be planning an attack against hotels in Kabul,\" the warning read. The Intercontinental Hotel is typically used for conferences and gatherings. In 2011, an attack at the hotel left 21 people dead, including nine attackers. A guest hiding in his hotel room told AFP he could hear the gunfire."
 
+    articleInformationList[0].print()
+    card = GenerateCardFromArticleInformation(articleInformationList[0])
+    InsertCardIntoHTMLDoc(card, True)
+    InsertCardIntoHTMLDoc(card)
+
+
+    OpenInWebBrowser()
     return
 
 main()
